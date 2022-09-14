51f050c2c2661cb63da9dd7ebb625ccfb4cfd7b3
denizburcu <denizburcua@gmail.com>
2022-09-14 11:42:50 +0300

    reformatted

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,21 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static Scanner  scanner = new Scanner(System.in);
+    private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         int[] myIntegers = getIntegers(5);
-        for(int i=0; i< myIntegers.length; i++) {
-            System.out.println("Element " +i+ ", typed value was " + myIntegers[i]);
+        for (int i = 0; i < myIntegers.length; i++) {
+            System.out.println("Element " + i + ", typed value was " + myIntegers[i]);
         }
-        System.out.println("The average is"  + getAverage(myIntegers));
+        System.out.println("The average is" + getAverage(myIntegers));
     }
 
     public static int[] getIntegers(int number) {
         System.out.println("Enter " + number + " integer values.\r");
         int[] values = new int[number];
 
-        for(int i=0; i< values.length; i++) {
+        for (int i = 0; i < values.length; i++) {
             values[i] = scanner.nextInt();
         }
 
@@ -25,9 +25,9 @@ public class Main {
 
     public static double getAverage(int[] array) {
         int sum = 0;
-        for(int i=0; i<array.length; i++) {
+        for (int i = 0; i < array.length; i++) {
             sum += array[i];
         }
-        return (double)sum / (double) array.length;
+        return (double) sum / (double) array.length;
     }
 }
