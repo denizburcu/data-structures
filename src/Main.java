90aa4329458002d98d6de4410771d07131bdc00a
denizburcu <denizburcua@gmail.com>
2022-09-19 18:41:52 +0300

    Array new method and tested

--- src/Main.java
+++ src/Main.java
@@ -5,29 +5,45 @@ public class Main {
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+
         int[] myIntegers = getIntegers(5);
-        for (int i = 0; i < myIntegers.length; i++) {
-            System.out.println("Element " + i + ", typed value was " + myIntegers[i]);
-        }
-        System.out.println("The average is" + getAverage(myIntegers));
+        int[] sorted = sortIntegers(myIntegers);
+        printArray(sorted);
     }
 
-    public static int[] getIntegers(int number) {
-        System.out.println("Enter " + number + " integer values.\r");
-        int[] values = new int[number];
-
-        for (int i = 0; i < values.length; i++) {
-            values[i] = scanner.nextInt();
-        }
-
-        return values;
-    }
-
-    public static double getAverage(int[] array) {
-        int sum = 0;
+    public static int[] getIntegers(int capacity) {
+        int[] array = new int[capacity];
+        System.out.println("Enter " + capacity + " integer values:\r");
         for (int i = 0; i < array.length; i++) {
-            sum += array[i];
+            array[i] = scanner.nextInt();
         }
-        return (double) sum / (double) array.length;
+        return array;
+    }
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Element " + i + " contents " + array[i]);
+        }
+    }
+    public static int[] sortIntegers(int[] array) {
+        int[]  sortedArray = new int[array.length];
+        for(int i=0; i<array.length; i++) {
+            sortedArray[i] = array[i];
+        }
+
+        boolean flag = true;
+        int temp;
+        while (flag){
+            flag = false;
+            for(int i=0; i<sortedArray.length-1; i++) {
+                if(sortedArray[i] < sortedArray[i+1]) {
+                    temp = sortedArray[i];
+                    sortedArray[i] = sortedArray[i+1];
+                    sortedArray[i+1] = temp;
+                    flag = true;
+                }
+            }
+        }
+        return sortedArray;
     }
 }
